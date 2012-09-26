a101c7f6f081f60a6e3dd739a5f238a200b9c2bf
Clayton Watts <cletusw@gmail.com>
2012-09-26 10:43:59 -0600

    Add missing function declaration

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -9,5 +9,6 @@ typedef struct {
 } PriorityQueue;
 
 PriorityQueue* newPriorityQueue(int length);
+void freePriorityQueue(PriorityQueue* q);
 void print(PriorityQueue* q);
 void enQ(PriorityQueue* q, int data, int priority);
